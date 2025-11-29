175a5af32d9aa41b633baa77b4c19cbce0684417
Ramit Roshann <ramitroshan777777@gmail.com>
2025-11-29 22:35:44 +0530

    update react-sandbox

---
+++ practice/react-sandbox/src/UserSearch.jsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+
+export default function UserSearch() {
+
+    // const [userId, setUserId] = useState("");
+
+    return(
+        <div>
+            <h2>User Search</h2>
+        </div>
+    );
+}
