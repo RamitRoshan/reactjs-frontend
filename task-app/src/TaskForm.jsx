fbd1b22a04812234e6b36ed97c0e4f1889f27593
Ramit Roshann <ramitroshan777777@gmail.com>
2025-12-10 16:34:33 +0530

    write axios post to update the list

--- task-app/src/TaskForm.jsx
+++ task-app/src/TaskForm.jsx
@@ -1,19 +1,44 @@
+import axios from "axios";
 import { useState } from "react";
 
-export default function TaskForm(props){  //TaskForm ({addTask})
+export default function TaskForm(props){  //TaskForm ({addTask}) . we can add TaskForm(props) 
 
     const [title, setTitle]  = useState('');
+    const [description, setDescription] = useState("");
+    // eslint-disable-next-line no-unused-vars
+    const [serverError, setServerError]  = useState(null);
+    
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const taskObj = {
-            id: Date.now(),
+            // id: Date.now(),
             title: title,
-            isCompleted: false
+            description: description,
+            status: 'pending'
         }
-        props.addTask(taskObj); // invoking the add task function
-        setTitle('');
+        
+        
+
+        // props.addTask(taskObj); // invoking the add task function
+        // setTitle('');
+
+        console.log('task obj', taskObj);
+        axios.post('http://localhost:7070/api/tasks/', taskObj)
+             .then((response) => {
+                console.log(response.data);
+                const data = response.data;
+                props.addTask(data);
+                setTitle('');
+                setDescription('');
+                setServerError(null);
+             })
+             .catch((err) => {
+                console.log(err);
+             })
+
     }
+
     return(
         <div>
             <h3>Add Task</h3>
@@ -25,6 +50,13 @@ export default function TaskForm(props){  //TaskForm ({addTask})
                       value={title}
                       onChange={e => setTitle(e.target.value)}
                     />
+                </div>
+                <div>
+                    <textarea 
+                      placeholder="enter description"
+                      value={description}
+                      onChange={e => setDescription(e.target.value)}>
+                    </textarea>
                 </div>
                 <div>
                     <input type="submit"/>
