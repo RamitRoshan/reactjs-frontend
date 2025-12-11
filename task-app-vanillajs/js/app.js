1da1ebb1658f15bd34314e397646a13a2b3de6de
Ramit Roshann <ramitroshan777777@gmail.com>
2025-12-11 16:11:57 +0530

    made new folder to print the data fetched from backend using vanilla js

--- task-app-vanillajs/js/app.js
+++ task-app-vanillajs/js/app.js
@@ -1,3 +1,13 @@
 const tasks = [];
 const countHandle = document.getElementById('count');
 countHandle.textContent = tasks.length;
+
+window.onload = function() {   //useEffect(() => {}, [])
+    axios.get('http://localhost:7070/api/tasks/')
+         .then((response) => {
+            console.log(response.data);
+         })
+         .catch((err) => {
+            console.log(err);
+         })
+}
