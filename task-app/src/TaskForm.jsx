eff6afb58d26e6a9f273f9f76d009692ea1a1d52
Ramit Roshann <ramitroshan777777@gmail.com>
2025-12-12 11:28:42 +0530

    implment extra local error variable so that it does not re render error everytime

--- task-app/src/TaskForm.jsx
+++ task-app/src/TaskForm.jsx
@@ -7,9 +7,14 @@ export default function TaskForm(props){  //TaskForm ({addTask}) . we can add Ta
     const [description, setDescription] = useState("");
     // eslint-disable-next-line no-unused-vars
     const [serverError, setServerError]  = useState(null);
+    const [clientErrors, setClientErrors] = useState({});
     
 
     const handleSubmit = (e) => {
+        // to avoid multiple re rendering we use local variable(error) otherwise we can use
+        //setClientErrors
+        const errors = {};
+
         e.preventDefault();
         const taskObj = {
             // id: Date.now(),
@@ -17,13 +22,26 @@ export default function TaskForm(props){  //TaskForm ({addTask}) . we can add Ta
             description: description,
             status: 'pending'
         }
-        
-        
-
-        // props.addTask(taskObj); // invoking the add task function
-        // setTitle('');
-
         console.log('task obj', taskObj);
+
+
+        if(title.trim().length == 0){
+            errors.title = "title is required";
+        }
+
+        if(description.trim().length == 0){
+            errors.description = "description is required";
+        }
+
+        console.log(errors);
+        if(Object.keys(errors).length != 0){
+            console.log('form errors', errors);
+            setClientErrors(errors);
+        } else {
+            console.log('no errors submit data to api');
+        }
+
+
         axios.post('http://localhost:7070/api/tasks/', taskObj)
              .then((response) => {
                 console.log(response.data);
@@ -32,9 +50,13 @@ export default function TaskForm(props){  //TaskForm ({addTask}) . we can add Ta
                 setTitle('');
                 setDescription('');
                 setServerError(null);
+                //to clean up the ui (housekeeping works -> like if we click on submibrn then again
+                // if it print title is required then this line will remove it)
+                setClientErrors({});
              })
              .catch((err) => {
-                console.log(err);
+                console.log(err.response.data);
+                setServerError(err.response.data);
              })
 
     }
@@ -50,6 +72,9 @@ export default function TaskForm(props){  //TaskForm ({addTask}) . we can add Ta
                       value={title}
                       onChange={e => setTitle(e.target.value)}
                     />
+                    {/* object.props is undefined i.e false and false && other is false
+                    i.e if clientError.tile is truthy then print print clientError in red color */}
+                    {clientErrors.title && <span style={{color: 'red'}}>{clientErrors.title}</span>}
                 </div>
                 <div>
                     <textarea 
@@ -57,6 +82,7 @@ export default function TaskForm(props){  //TaskForm ({addTask}) . we can add Ta
                       value={description}
                       onChange={e => setDescription(e.target.value)}>
                     </textarea>
+                    {clientErrors.description && <span style={{ color: 'red'}}>{clientErrors.description}</span>}
                 </div>
                 <div>
                     <input type="submit"/>
