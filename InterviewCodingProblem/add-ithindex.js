128ab8eab0621c2f09d17a530cbe8275b86c4853
Ramit Roshann <ramitroshan777777@gmail.com>
2025-12-15 14:53:31 +0530

    added new folder of interview coding and solved multiple interview coding questions

--- InterviewCodingProblem/add-ithindex.js
+++ InterviewCodingProblem/add-ithindex.js
@@ -6,10 +6,10 @@ here, u have to add 100 at index-2 without adding any new array(filter or map), 
 final o/p:  arr = [10, 20, 100, 30, 40]
 */
 
-let arr = [10, 20, 30, 40];
+const arr = [10, 20, 30, 40];
 
-let index = 2;
-let value = 100;
+const index = 2;
+const value = 100;
 
 // move element one steps right to add "100" at index-2 (starting from end)
 for(let i=arr.length; i > index; i--){
@@ -23,4 +23,14 @@ console.log(arr);
 
 
 
-// let arr = [10, 20, 30]; add 40 without using push methods.
+/* let arr = [10, 20, 30]; add 40 without using push methods.
+o/p: [10, 20, 30, 40]
+*/
+
+let array = [10, 20, 30];
+
+// arr.length gives the next empty index (here = 3)
+array[array.length] = 40;
+
+// final array
+console.log(array); 
