261e332956a85d97b00721043496f07f61f87855
Ramit Roshann <ramitroshan777777@gmail.com>
2025-12-18 17:40:24 +0530

    made new folder for react-router-dom for implementation

---
+++ router-dom-practice/src/App.jsx
@@ -0,0 +1,58 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
+
+
+// import "./styles.css";
+// import Home from "./Home";
+// import About from "./About";
+// import Contact from "./Contact";
+// import Services from "./Services";
+// import Login from "./Login";
+// import { Link, Routes, Route } from "react-router-dom";
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello CodeSandbox</h1>
+
+//       <ul className="top-nav">
+//         <li>
+//           <Link to="/">Home</Link>
+//         </li>
+//         <li>
+//           <Link to="/about">About</Link>
+//         </li>
+//         <li>
+//           <Link to="/services">Services</Link>
+//         </li>
+//         <li>
+//           <Link to="/contact">Contact</Link>
+//         </li>
+//         <li>
+//           <Link to="/login">Login</Link>
+//         </li>
+//       </ul>
+//       {/* <Home />
+//       <About />
+//       <Contact /> */}
+
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/services" element={<Services />} />
+//         <Route path="/contact" element={<Contact />} />
+//         <Route path="/login" element={<Login />} />
+//       </Routes>
+//     </div>
+//   );
+// }
