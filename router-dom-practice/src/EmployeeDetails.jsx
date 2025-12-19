c65ede3be9cc94b84b2a69eac592ee5e248e7155
Ramit Roshann <ramitroshan777777@gmail.com>
2025-12-19 22:06:13 +0530

    update router-dom-practice

---
+++ router-dom-practice/src/EmployeeDetails.jsx
@@ -0,0 +1,22 @@
+
+export default function EmployeeDetails() {
+
+    //when there is  no data available then we use null
+    
+    return (
+        <div>
+            <h3>Employee Details </h3>
+        </div>
+    );
+}
+
+
+
+/* using axios fetch the record from the server with the help of the id
+    and console it.
+
+    create a state variable = user and store the response into the state variable
+
+    display the user info like name , email,beloww inside jsx
+
+*/
