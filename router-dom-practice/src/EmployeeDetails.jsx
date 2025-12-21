aa4cf685805083662c15736d1f426e2beea280fc
Ramit Roshann <ramitroshan777777@gmail.com>
2025-12-22 01:16:45 +0530

    update router-dom-practice

--- router-dom-practice/src/EmployeeDetails.jsx
+++ router-dom-practice/src/EmployeeDetails.jsx
@@ -1,13 +1,54 @@
 
-export default function EmployeeDetails() {
-
+// export default function EmployeeDetails() {
     //when there is  no data available then we use null
-    
-    return (
+
+//     return (
+//         <div>
+//             <h3>Employee Details </h3>
+//         </div>
+//     );
+// }
+
+
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+export default function EmployeeDetails() {
+  const { id } = useParams();
+
+  //when there is  no data available then we use null
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((response) => {
+        setUser(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+
+    return () => {
+      console.log("component unmounted");
+    };
+  }, [id]); //when id updated then it do rerender
+
+  return (
+    <div>
+      <h3>Employee Details - {id}</h3>
+
+      {user && (
         <div>
-            <h3>Employee Details </h3>
+          <p>Name: {user.name}</p>
+          <p>Email: {user.email}</p>
+          <p>Company: {user.company.name}</p>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 
