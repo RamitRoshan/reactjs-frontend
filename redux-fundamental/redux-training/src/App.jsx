ac5e72b0cbbd0356c0216ec5f7cd446bc9548d45
Ramit Roshann <ramitroshan777777@gmail.com>
2026-02-10 15:01:22 +0530

    writing code for actions, reducers and store in redux

---
+++ redux-fundamental/redux-training/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <h2>Redux Trainnig</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ redux-fundamental/redux-training/src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import './index.css'
+import App from './App.jsx'
+import configureStore from "./store/configureStore"
+import { increment, decrement, incrementBy } from './actions/countActions'
+
+const store = configureStore();
+// console.log(store);
+
+console.log('state', store.getState())
+
+store.subscribe(() => {
+  console.log('state updated', store.getState())
+})
+
+
+store.dispatch(increment())
+
+createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+  </>,
+)
